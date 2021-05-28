02b0d44baa7bf7ab43a2c87018615bfd92b3c58e
caarlosdamian <dapecarlos@gmail.com>
2021-05-27 22:39:34 -0500

    add Menu component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Menu from "./components/pages/Menu";
 import Home from './components/pages/Home'
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
@@ -11,6 +12,7 @@ function App() {
         <Navbar />
         <Switch>
           <Route path="/" exact component={Home} />
+          <Route path="/menu" exact component={Menu} />
         </Switch>
         <Footer/>
       </Router>
