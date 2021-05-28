1a370b34bc751abb5e8fa20917f88f7a6b1a23f5
caarlosdamian <dapecarlos@gmail.com>
2021-05-27 22:46:29 -0500

    add About component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Menu from "./components/pages/Menu";
-import Home from './components/pages/Home'
+import Home from './components/pages/Home';
+import About from './components/pages/About'
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/menu" exact component={Menu} />
+          <Route path="/about" exact component={About} />
         </Switch>
         <Footer/>
       </Router>
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,11 +1,32 @@
-import React from 'react'
-
+import React from "react";
+import MultiplePizzas from "../../assets/multiplePizzas.jpeg";
+import "../../styles/About.css";
 function About() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div className="about">
+      <div
+        className="aboutTop"
+        style={{ backgroundImage: `url(${MultiplePizzas})` }}
+      >
+
+       
+      </div>
+      <div className="aboutBottom">
+      <h1>ABOUT US</h1>
+      <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.
+        </p>
+      </div>
+    </div>
+  );
 }
 
-export default About
+export default About;
