0e457897120df4e8814082c95714abc2a1e129d2
caarlosdamian <dapecarlos@gmail.com>
2021-05-27 22:57:07 -0500

    add Contact component

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Menu from "./components/pages/Menu";
-import Home from './components/pages/Home';
-import About from './components/pages/About'
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Contact from "./components/pages/Contact";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
@@ -15,8 +16,9 @@ function App() {
           <Route path="/" exact component={Home} />
           <Route path="/menu" exact component={Menu} />
           <Route path="/about" exact component={About} />
+          <Route path="/contact" exact component={Contact} />
         </Switch>
-        <Footer/>
+        <Footer />
       </Router>
     </div>
   );
